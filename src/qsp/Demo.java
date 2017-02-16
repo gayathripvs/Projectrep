de6757e1ae70204c0fce0140d6fe263094a095c0
VAIO <VAIO@VAIO-VAIO>
2017-02-16 22:55:33 +0530

    Uploading Framework

---
+++ src/qsp/Demo.java
@@ -0,0 +1,32 @@
+package qsp;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Demo
+{
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void m1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+	}
+		@AfterMethod
+		public void m2()
+		{
+			driver.close();
+		
+	     }
+		@Test
+		public void m3()
+		{
+			Reporter.log("hi",true);
+		}
+
+}
